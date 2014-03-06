7ad9678cc7ee4abfedc814b26a8ddf38ada40cab
hcz <h.cz@qq.com>
2014-03-07 00:39:19 +0800

    move executable to /output

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 splint bootstrap/_compile.c -checks
 splint wrap/main.c          -checks
-clang bootstrap/_compile.c  -o build/bootstrap.o -c -std=c99 -pedantic -Wall -Wextra
-clang wrap/main.c           -o build/main.o      -c -std=c99 -pedantic -Wall -Wextra
+clang bootstrap/_compile.c  -o output/bootstrap.o -c -std=c99 -pedantic -Wall -Wextra
+clang wrap/main.c           -o output/main.o      -c -std=c99 -pedantic -Wall -Wextra
 
-clang build/*.o -o build/zin -lgc
-./build/zin
+clang output/*.o -o output/zin -lgc
+./output/zin
