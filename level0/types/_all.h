c92b27dfd0d0ae52663da78eafeb4dd7da76bde8
hcz <h.cz@qq.com>
2014-03-09 01:06:06 +0800

    add enum, fix

--- level0/types/_all.h
+++ level0/types/_all.h
@@ -1,5 +1,11 @@
 #include "../../base/_all.h"
 
+enum {
+    #define ZIN_MODE ZIN_MODE_ENUM
+    #include "_list.inc"
+    #undef ZIN_MODE
+};
+
 #define ZIN_MODE ZIN_MODE_HEADER_READ
 #include "_list.inc"
 #undef ZIN_MODE
