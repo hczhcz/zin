740e0adf883c60d04350c7a21de2b01807fbb3d5
hcz <h.cz@qq.com>
2014-03-10 00:40:48 +0800

    rename files

--- level0/types/_all.h
+++ level0/types/_all.h
@@ -2,16 +2,16 @@
 
 enum {
     #define ZIN_MODE ZIN_MODE_ID
-    #include "_list.inc"
+    #include "_list.h"
     #undef ZIN_MODE
 };
 
 #define ZIN_MODE ZIN_MODE_HEADER_READ
-#include "_list.inc"
+#include "_list.h"
 #undef ZIN_MODE
 
 #ifdef ZIN_ENABLE_WRITE
     #define ZIN_MODE ZIN_MODE_HEADER_WRITE
-    #include "_list.inc"
+    #include "_list.h"
     #undef ZIN_MODE
 #endif
--- level0/types/_compile.c
+++ level0/types/_compile.c
@@ -1,11 +1,11 @@
 #include "_all.h"
 
 #define ZIN_MODE ZIN_MODE_READ
-#include "_list.inc"
+#include "_list.h"
 #undef ZIN_MODE
 
 #ifdef ZIN_ENABLE_WRITE
     #define ZIN_MODE ZIN_MODE_WRITE
-    #include "_list.inc"
+    #include "_list.h"
     #undef ZIN_MODE
 #endif
