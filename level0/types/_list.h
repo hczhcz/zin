8130c46e9d7645425947df714073f2cef272e4f0
hcz <h.cz@qq.com>
2014-03-11 01:38:09 +0800

    move initializers' name to _list.h

--- level0/types/_list.h
+++ level0/types/_list.h
@@ -1,3 +1,8 @@
+#define zr_ZIN_CURRENT_INIT zrinit_types
+#define zw_ZIN_CURRENT_INIT zwinit_types
+#define za_ZIN_CURRENT_INIT zainit_types
+#define ze_ZIN_CURRENT_INIT zeinit_types
+
 #include "_init.zc"
 #include "arr.zc"
 #include "atom.zc"
@@ -7,3 +12,8 @@
 #include "func.zc"
 #include "int.zc"
 #include "str.zc"
+
+#undef zr_ZIN_CURRENT_INIT
+#undef zw_ZIN_CURRENT_INIT
+#undef za_ZIN_CURRENT_INIT
+#undef ze_ZIN_CURRENT_INIT
