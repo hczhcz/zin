d05f104b4973baa27a92148f288b54b52c375651
hcz <h.cz@qq.com>
2014-04-01 21:25:34 +0800

    add data construction macros

--- base/data.h
+++ base/data.h
@@ -7,6 +7,7 @@ typedef char ztchar;
 typedef unsigned long ztid;
 typedef unsigned long ztsize;
 
+////////////////////////////////////////////////////////////////
 // Head: [type]
 typedef struct {
     ztid layout;
@@ -15,14 +16,30 @@ typedef struct {
 
 typedef zd *pzd;
 
+#define ZIN_NEW(target, layout, type) {\
+    target->layout = ZIN_ID(layout);\
+    target->type = ZIN(type);\
+}
+
+////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////
     // Atom: [head]
+
     typedef struct {
         zd head;
     } zdatom;
 
     typedef zdatom *pzdatom;
 
+    #define ZIN_NEW_ATOM(target, type) {\
+        target = ZIN_ALLOCATE_NO_OUT(sizeof(zdatom));\
+        ZIN_NEW(target, atom, type)\
+    }
+
+    ////////////////////////////////////////////////////////////////
     // Int: [head] [int]
+
     typedef struct {
         zd head;
         ztint value;
@@ -30,7 +47,14 @@ typedef zd *pzd;
 
     typedef zdint *pzdint;
 
+    #define ZIN_NEW_INT(target, type) {\
+        target = ZIN_ALLOCATE_NO_OUT(sizeof(zdint));\
+        ZIN_NEW(target, int, type)\
+    }
+
+    ////////////////////////////////////////////////////////////////
     // Str: [head] [memsize (bit)] [realsize] [char] ...
+
     typedef struct {
         zd head;
         ztsize memsize;
@@ -40,7 +64,14 @@ typedef zd *pzd;
 
     typedef zdstr *pzdstr;
 
+    #define ZIN_NEW_STR(target, type) {\
+        target = ZIN_ALLOCATE_NO_OUT(sizeof(zdstr));\
+        ZIN_NEW(target, str, type)\
+    }
+
+    ////////////////////////////////////////////////////////////////
     // Code: [head] [ptr]
+
     typedef struct {
         zd head;
         pzd param;
@@ -48,8 +79,15 @@ typedef zd *pzd;
 
     typedef zdcode *pzdcode;
 
+    #define ZIN_NEW_CODE(target, type) {\
+        target = ZIN_ALLOCATE(sizeof(zdcode));\
+        ZIN_NEW(target, code, type)\
+    }
+
+    ////////////////////////////////////////////////////////////////
     // Arr: [head] [memsize (bit)] [front] [back] [ptr] ...
     // 0: nil, 1: nil, 2=front: ptr, 3: ptr, 4: ptr, 5=back: nil, ...
+
     typedef struct {
         zd head;
         ztsize memsize;
@@ -60,13 +98,20 @@ typedef zd *pzd;
 
     typedef zdarr *pzdarr;
 
+    #define ZIN_NEW_ARR(target, type) {\
+        target = ZIN_ALLOCATE(sizeof(zdarr));\
+        ZIN_NEW(target, arr, type)\
+    }
+
+    ////////////////////////////////////////////////////////////////
+    // Dict: [head] [memsize (bit)] [realsize] [ptr] ...
+
         // Node of dict
         typedef struct {
             pzd index;
             pzd data;
         } zddictnode;
 
-    // Dict: [head] [memsize (bit)] [realsize] [ptr] ...
     typedef struct {
         zd head;
         ztsize memsize;
@@ -76,7 +121,14 @@ typedef zd *pzd;
 
     typedef zddict *pzddict;
 
+    #define ZIN_NEW_DICT(target, type) {\
+        target = ZIN_ALLOCATE(sizeof(zddict));\
+        ZIN_NEW(target, dict, type)\
+    }
+
+    ////////////////////////////////////////////////////////////////
     // Context: [head] [callbuf] [libbuf]
+
     typedef struct {
         zd head;
         pzdarr callbuf;
@@ -85,7 +137,14 @@ typedef zd *pzd;
 
     typedef zdcontext *pzdcontext;
 
+    #define ZIN_NEW_CONTEXT(target, type) {\
+        target = ZIN_ALLOCATE(sizeof(zdcontext));\
+        ZIN_NEW(target, context, type)\
+    }
+
+    ////////////////////////////////////////////////////////////////
     // Func: [head] [funcr] [funcw]
+
     typedef struct {
         zd head;
         ZIN_PROTO_PR(funcr);
@@ -93,5 +152,13 @@ typedef zd *pzd;
     } zdfunc;
 
     typedef zdfunc *pzdfunc;
+
+    #define ZIN_NEW_FUNC(target, type) {\
+        /* target = ZIN_ALLOCATE_NO_OUT(sizeof(zdfunc)); */\
+        target = ZIN_ALLOCATE(sizeof(zdfunc));\
+        ZIN_NEW(target, func, type)\
+    }
+
+    ////////////////////////////////////////////////////////////////
 
 #endif
--- base/macro.h
+++ base/macro.h
@@ -1,6 +1,15 @@
 #ifndef ZIN_MACRO_H
 #define ZIN_MACRO_H
 
+// za: id allocator
+// ze: id
+
+// zd: zin data
+// zt: simple data
+
+// zr: read func
+// zw: write func
+
 // Inclusion modes
 #define ZIN_MODE_ID 1
 #define ZIN_MODE_HEADER_READ 2
@@ -32,15 +41,14 @@
 
 // Memory management
 #define ZIN_MEM_INIT() GC_INIT()
-#define ZIN_CREATE(size) GC_malloc(size)
-#define ZIN_CREATE_NO_OUT(size) GC_malloc_atomic(size)
-#define ZIN_CREATE_NO_IN(size) GC_malloc_ignore_off_page(size)
+#define ZIN_ALLOCATE(size) GC_malloc(size)
+#define ZIN_ALLOCATE_NO_OUT(size) GC_malloc_atomic(size)
+#define ZIN_ALLOCATE_NO_IN(size) GC_malloc_ignore_off_page(size)
 #define ZIN_RESIZE(obj, size) GC_realloc(obj, size)
 #define ZIN_FREE(obj) GC_free(obj)
 
-// Memory management with type
+// Casting
 #define ZIN_AS(type, data) ((pzd##type) data)
-#define ZIN_CREATE_AS(type, size) ZIN_CREATE(sizeof(zd##type))
 
 // Calling
 #define ZIN_STATIC_READ(name, caller) ((zr##name) (zin, caller))
