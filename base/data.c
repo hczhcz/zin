6152e80ed9004bf4d454649e516f6bb214f44a90
hcz <h.cz@qq.com>
2014-05-05 16:35:49 +0800

    change structs' typedef, add func pointer to zd

--- base/data.c
+++ base/data.c
@@ -1,62 +1,69 @@
 #include "_all.h"
 
-pzd zin_gen_atom(ztid tolayout, ztid totype) {
-    pzdatom result = ZIN_ALLOCATE_NO_OUT(sizeof(zdatom));
+pzd zin_gen_atom(ztid tolayout, ztid totype, pztfunc tofunc) {
+    pzdatom result = ZIN_ALLOCATE_NO_OUT(sizeof(struct zdatom));
     result->head.layout = tolayout;
     result->head.type = totype;
+    result->head.func = tofunc;
     return (pzd)result;
 }
 
-pzd zin_gen_int(ztid tolayout, ztid totype, ztint tovalue) {
-    pzdint result = ZIN_ALLOCATE_NO_OUT(sizeof(zdint));
+pzd zin_gen_int(ztid tolayout, ztid totype, pztfunc tofunc, ztint tovalue) {
+    pzdint result = ZIN_ALLOCATE_NO_OUT(sizeof(struct zdint));
     result->head.layout = tolayout;
     result->head.type = totype;
+    result->head.func = tofunc;
     result->value = tovalue;
     return (pzd)result;
 }
 
-pzd zin_gen_str(ztid tolayout, ztid totype, ztsize tomemsize) {
-    pzdstr result = ZIN_ALLOCATE_NO_OUT(sizeof(zdstr) + (sizeof(ztchar) << tomemsize));
+pzd zin_gen_str(ztid tolayout, ztid totype, pztfunc tofunc, ztsize tomemsize) {
+    pzdstr result = ZIN_ALLOCATE_NO_OUT(sizeof(struct zdstr) + (sizeof(ztchar) << tomemsize));
     result->head.layout = tolayout;
     result->head.type = totype;
+    result->head.func = tofunc;
     result->memsize = tomemsize;
     result->front = 0;
     result->back = 0;
     return (pzd)result;
 }
 
-pzd zin_gen_ptr(ztid tolayout, ztid totype, pzd todata) {
-    pzdptr result = ZIN_ALLOCATE(sizeof(zdptr));
+pzd zin_gen_ptr(ztid tolayout, ztid totype, pztfunc tofunc, pzd todata) {
+    pzdptr result = ZIN_ALLOCATE(sizeof(struct zdptr));
     result->head.layout = tolayout;
     result->head.type = totype;
+    result->head.func = tofunc;
     result->data = todata;
     return (pzd)result;
 }
 
-pzd zin_gen_pair(ztid tolayout, ztid totype, pzd toleft, pzd toright) {
-    pzdpair result = ZIN_ALLOCATE(sizeof(zdpair));
+pzd zin_gen_pair(ztid tolayout, ztid totype, pztfunc tofunc, pzd toleft, pzd toright) {
+    pzdpair result = ZIN_ALLOCATE(sizeof(struct zdpair));
     result->head.layout = tolayout;
     result->head.type = totype;
+    result->head.func = tofunc;
     result->left = toleft;
     result->right = toright;
     return (pzd)result;
 }
 
-pzd zin_gen_arr(ztid tolayout, ztid totype, ztsize tomemsize) {
-    pzdarr result = ZIN_ALLOCATE(sizeof(zdarr) + (sizeof(pzd) << tomemsize));
+pzd zin_gen_arr(ztid tolayout, ztid totype, pztfunc tofunc, ztsize tomemsize) {
+    pzdarr result = ZIN_ALLOCATE(sizeof(struct zdarr) + (sizeof(pzd) << tomemsize));
     result->head.layout = tolayout;
     result->head.type = totype;
+    result->head.func = tofunc;
     result->memsize = tomemsize;
     result->front = 0;
     result->back = 0;
     return (pzd)result;
 }
 
-pzd zin_gen_dict(ztid tolayout, ztid totype, ztsize tomemsize) {
+pzd zin_gen_dict(ztid tolayout, ztid totype, pztfunc tofunc, ztsize tomemsize) {
     ztsize index;
-    pzddict result = ZIN_ALLOCATE(sizeof(zddict) + (sizeof(zddictnode) << tomemsize));
+    pzddict result = ZIN_ALLOCATE(sizeof(struct zddict) + (sizeof(ztidnode) << tomemsize));
     result->head.layout = tolayout;
     result->head.type = totype;
+    result->head.func = tofunc;
     result->memsize = tomemsize;
     result->realsize = 0;
     for (index = 0; index < (1u << tomemsize); ++index) {
@@ -65,20 +72,22 @@ pzd zin_gen_dict(ztid tolayout, ztid totype, ztsize tomemsize) {
     return (pzd)result;
 }
 
-pzd zin_gen_context(ztid tolayout, ztid totype, pzdarr tocallbuf, pzdarr tolibbuf) {
-    pzdcontext result = ZIN_ALLOCATE(sizeof(zdcontext));
+pzd zin_gen_context(ztid tolayout, ztid totype, pztfunc tofunc, pzdarr tocallbuf, pzdarr tolibbuf) {
+    pzdcontext result = ZIN_ALLOCATE(sizeof(struct zdcontext));
     result->head.layout = tolayout;
     result->head.type = totype;
+    result->head.func = tofunc;
     result->callbuf = tocallbuf;
     result->libbuf = tolibbuf;
     return (pzd)result;
 }
 
-pzd zin_gen_func(ztid tolayout, ztid totype, ZIN_PROTO_PR(tofuncr), ZIN_PROTO_PW(tofuncw)) {
-    pzdfunc result = ZIN_ALLOCATE(sizeof(zdfunc));
+pzd zin_gen_func(ztid tolayout, ztid totype, pztfunc tofunc, ZIN_PROTO_PR(tofuncr), ZIN_PROTO_PW(tofuncw)) {
+    pzdfunc result = ZIN_ALLOCATE(sizeof(struct zdfunc));
     result->head.layout = tolayout;
     result->head.type = totype;
-    result->funcr = tofuncr;
-    result->funcw = tofuncw;
+    result->head.func = tofunc;
+    result->func.funcr = tofuncr;
+    result->func.funcw = tofuncw;
     return (pzd)result;
 }
--- base/data.h
+++ base/data.h
@@ -8,142 +8,173 @@ typedef unsigned int ztid;
 typedef unsigned int ztsize;
 
 ////////////////////////////////////////////////////////////////
-// Head: [type]
+
+struct zd;
+struct zdatom;
+struct zdint;
+struct zdstr;
+struct zdptr;
+struct zdpair;
+struct zdarr;
+struct zddict;
+struct zdcontext;
+struct zdfunc;
+typedef struct zd *pzd;
+typedef struct zdatom *pzdatom;
+typedef struct zdint *pzdint;
+typedef struct zdstr *pzdstr;
+typedef struct zdptr *pzdptr;
+typedef struct zdpair *pzdpair;
+typedef struct zdarr *pzdarr;
+typedef struct zddict *pzddict;
+typedef struct zdcontext *pzdcontext;
+typedef struct zdfunc *pzdfunc;
+
+////////////////////////////////////////////////////////////////
+// IdNode: [id] [ptr]
+
 typedef struct {
+    ztid index;
+    pzd data;
+} ztidnode;
+
+////////////////////////////////////////////////////////////////
+// Func: [funcr] [funcw]
+
+typedef struct {
+    ZIN_PROTO_PR(funcr);
+    ZIN_PROTO_PW(funcw);
+} ztfunc;
+
+typedef ztfunc *pztfunc;
+
+////////////////////////////////////////////////////////////////
+// Head: [layout] [type]
+struct zd {
     ztid layout;
     ztid type;
-} zd;
-
-typedef zd *pzd;
+    pztfunc func;
+};
 
 ////////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////////
     // Atom: [head]
 
-    typedef struct {
-        zd head;
-    } zdatom;
+    struct zdatom {
+        struct zd head;
+    };
 
-    typedef zdatom *pzdatom;
-
-    pzd zin_gen_atom(ztid tolayout, ztid totype);
-    #define ZIN_NEW_ATOM(totype) zin_gen_atom(ZIN_ID(atom), ZIN_ID(totype))
+    pzd zin_gen_atom(ztid tolayout, ztid totype, pztfunc tofunc);
+    #define ZIN_NEW_ATOM(totype) zin_gen_atom(ZIN_ID(atom), ZIN_ID(totype), zin->head.func)
+    #define ZIN_NEW_ATOM_BIND(totype, tofunc) zin_gen_atom(ZIN_ID(atom), ZIN_ID(totype), tofunc)
 
     ////////////////////////////////////////////////////////////////
     // Int: [head] [int]
 
-    typedef struct {
-        zd head;
+    struct zdint {
+        struct zd head;
         ztint value;
     } zdint;
 
-    typedef zdint *pzdint;
-    pzd zin_gen_int(ztid tolayout, ztid totype, ztint tovalue);
-    #define ZIN_NEW_INT(totype, tovalue) zin_gen_int(ZIN_ID(int), ZIN_ID(totype), tovalue)
+    pzd zin_gen_int(ztid tolayout, ztid totype, pztfunc tofunc, ztint tovalue);
+    #define ZIN_NEW_INT(totype, tovalue) zin_gen_int(ZIN_ID(int), ZIN_ID(totype), zin->head.func, tovalue)
+    #define ZIN_NEW_INT_BIND(totype, tofunc, tovalue) zin_gen_int(ZIN_ID(int), ZIN_ID(totype), tofunc, tovalue)
 
     ////////////////////////////////////////////////////////////////
     // Str: [head] [memsize (bit)] [front] [back] [char] ...
 
-    typedef struct {
-        zd head;
+    struct zdstr {
+        struct zd head;
         ztsize memsize;
         ztsize front;
         ztsize back;
         ztchar strdata[];
-    } zdstr;
+    };
 
-    typedef zdstr *pzdstr;
-    pzd zin_gen_str(ztid tolayout, ztid totype, ztsize tomemsize);
-    #define ZIN_NEW_STR(totype, tomemsize) zin_gen_str(ZIN_ID(str), ZIN_ID(totype), tomemsize)
+    pzd zin_gen_str(ztid tolayout, ztid totype, pztfunc tofunc, ztsize tomemsize);
+    #define ZIN_NEW_STR(totype, tomemsize) zin_gen_str(ZIN_ID(str), ZIN_ID(totype), zin->head.func, tomemsize)
+    #define ZIN_NEW_STR_BIND(totype, tofunc, tomemsize) zin_gen_str(ZIN_ID(str), ZIN_ID(totype), tofunc, tomemsize)
 
     ////////////////////////////////////////////////////////////////
     // Ptr: [head] [ptr]
 
-    typedef struct {
-        zd head;
+    struct zdptr {
+        struct zd head;
         pzd data;
-    } zdptr;
+    };
 
-    typedef zdptr *pzdptr;
-    pzd zin_gen_ptr(ztid tolayout, ztid totype, pzd todata);
-    #define ZIN_NEW_PTR(totype, todata) zin_gen_ptr(ZIN_ID(ptr), ZIN_ID(totype), todata)
-
+    pzd zin_gen_ptr(ztid tolayout, ztid totype, pztfunc tofunc, pzd todata);
+    #define ZIN_NEW_PTR(totype, todata) zin_gen_ptr(ZIN_ID(ptr), ZIN_ID(totype), zin->head.func, todata)
+    #define ZIN_NEW_PTR_BIND(totype, tofunc, todata) zin_gen_ptr(ZIN_ID(ptr), ZIN_ID(totype), tofunc, todata)
 
     ////////////////////////////////////////////////////////////////
     // Pair: [head] [ptr] [ptr]
 
-    typedef struct {
-        zd head;
+    struct zdpair {
+        struct zd head;
         pzd left;
         pzd right;
-    } zdpair;
+    };
 
-    typedef zdpair *pzdpair;
-    pzd zin_gen_pair(ztid tolayout, ztid totype, pzd toleft, pzd toright);
-    #define ZIN_NEW_PAIR(totype, toleft, toright) zin_gen_pair(ZIN_ID(pair), ZIN_ID(totype), toleft, toright)
+    pzd zin_gen_pair(ztid tolayout, ztid totype, pztfunc tofunc, pzd toleft, pzd toright);
+    #define ZIN_NEW_PAIR(totype, toleft, toright) zin_gen_pair(ZIN_ID(pair), ZIN_ID(totype), zin->head.func, toleft, toright)
+    #define ZIN_NEW_PAIR_BIND(totype, tofunc, toleft, toright) zin_gen_pair(ZIN_ID(pair), ZIN_ID(totype), tofunc, toleft, toright)
 
     ////////////////////////////////////////////////////////////////
     // Arr: [head] [memsize (bit)] [front] [back] [ptr] ...
     // 0: nil, 1: nil, 2=front: ptr, 3: ptr, 4: ptr, 5=back: nil, ...
 
-    typedef struct {
-        zd head;
+    struct zdarr {
+        struct zd head;
         ztsize memsize;
         ztsize front;
         ztsize back;
         pzd buf[];
-    } zdarr;
+    };
 
-    typedef zdarr *pzdarr;
-    pzd zin_gen_arr(ztid tolayout, ztid totype, ztsize tomemsize);
-    #define ZIN_NEW_ARR(totype, tomemsize) zin_gen_arr(ZIN_ID(arr), ZIN_ID(totype), memsize)
+    pzd zin_gen_arr(ztid tolayout, ztid totype, pztfunc tofunc, ztsize tomemsize);
+    #define ZIN_NEW_ARR(totype, tomemsize) zin_gen_arr(ZIN_ID(arr), ZIN_ID(totype), zin->head.func, memsize)
+    #define ZIN_NEW_ARR_BIND(totype, tofunc, tomemsize) zin_gen_arr(ZIN_ID(arr), ZIN_ID(totype), tofunc, memsize)
 
     ////////////////////////////////////////////////////////////////
     // Dict: [head] [memsize (bit)] [realsize] [[index] [ptr]] ...
 
-        // Node of dict
-        typedef struct {
-            ztint index;
-            pzd data;
-        } zddictnode;
-
-    typedef struct {
-        zd head;
+    struct zddict {
+        struct zd head;
         ztsize memsize;
         ztsize realsize;
-        zddictnode node[];
-    } zddict;
+        ztidnode node[];
+    };
 
-    typedef zddict *pzddict;
-    pzd zin_gen_dict(ztid tolayout, ztid totype, ztsize tomemsize);
-    #define ZIN_NEW_DICT(totype, tomemsize) zin_gen_dict(ZIN_ID(dict), ZIN_ID(totype), memsize)
+    pzd zin_gen_dict(ztid tolayout, ztid totype, pztfunc tofunc, ztsize tomemsize);
+    #define ZIN_NEW_DICT(totype, tomemsize) zin_gen_dict(ZIN_ID(dict), ZIN_ID(totype), zin->head.func, memsize)
+    #define ZIN_NEW_DICT_BIND(totype, tofunc, tomemsize) zin_gen_dict(ZIN_ID(dict), ZIN_ID(totype), tofunc, memsize)
 
     ////////////////////////////////////////////////////////////////
     // Context: [head] [callbuf] [libbuf]
 
-    typedef struct {
-        zd head;
+    struct zdcontext {
+        struct zd head;
         pzdarr callbuf;
         pzdarr libbuf;
-    } zdcontext;
+    };
 
-    typedef zdcontext *pzdcontext;
-    pzd zin_gen_context(ztid tolayout, ztid totype, pzdarr tocallbuf, pzdarr tolibbuf);
-    #define ZIN_NEW_CONTEXT(totype, tocallbuf, tolibbuf) zin_gen_context(ZIN_ID(context), ZIN_ID(totype), tocallbuf, tolibbuf)
+    pzd zin_gen_context(ztid tolayout, ztid totype, pztfunc tofunc, pzdarr tocallbuf, pzdarr tolibbuf);
+    #define ZIN_NEW_CONTEXT(totype, tocallbuf, tolibbuf) zin_gen_context(ZIN_ID(context), ZIN_ID(totype), zin->head.func, tocallbuf, tolibbuf)
+    #define ZIN_NEW_CONTEXT_BIND(totype, tofunc, tocallbuf, tolibbuf) zin_gen_context(ZIN_ID(context), ZIN_ID(totype), tofunc, tocallbuf, tolibbuf)
 
     ////////////////////////////////////////////////////////////////
     // Func: [head] [funcr] [funcw]
 
-    typedef struct {
-        zd head;
-        ZIN_PROTO_PR(funcr);
-        ZIN_PROTO_PW(funcw);
-    } zdfunc;
+    struct zdfunc {
+        struct zd head;
+        ztfunc func;
+    };
 
-    typedef zdfunc *pzdfunc;
-    pzd zin_gen_func(ztid tolayout, ztid totype, ZIN_PROTO_PR(tofuncr), ZIN_PROTO_PW(tofuncw));
-    #define ZIN_NEW_FUNC(totype, tofuncr, tofuncw) zin_gen_func(ZIN_ID(func), ZIN_ID(totype), tofuncr, tofuncw)
+    pzd zin_gen_func(ztid tolayout, ztid totype, pztfunc tofunc, ZIN_PROTO_PR(tofuncr), ZIN_PROTO_PW(tofuncw));
+    #define ZIN_NEW_FUNC(totype, tofuncr, tofuncw) zin_gen_func(ZIN_ID(func), ZIN_ID(totype), zin->head.func, tofuncr, tofuncw)
+    #define ZIN_NEW_FUNC_BIND(totype, tofunc, tofuncr, tofuncw) zin_gen_func(ZIN_ID(func), ZIN_ID(totype), tofunc, tofuncr, tofuncw)
 
     ////////////////////////////////////////////////////////////////
 
